064372cd73e7dedeeb46e6a3a65b499b2558a6b7
navisraditya <navisraditya@gmail.com>
2024-03-10 10:53:20 +0700

    background z-index set

--- Scenes/Background_change/Background.gd
+++ Scenes/Background_change/Background.gd
@@ -1,11 +1,14 @@
 extends ColorRect
 
-func _ready():
-	self.color = Color(1.0, 1.0, 1.0, 1.0)
+var isWhite = true
 
 func change_background_color():
-	self.color = Color(randf(), randf(), randf(), 1.0)
-
+	if isWhite == true:
+		self.color = Color(0, 0, 0, 1.0)
+		isWhite = false
+	else:
+		self.color = Color(1.0, 1.0, 1.0, 1.0)
+		isWhite = true
 
 func _input(event):
 	if event.is_action_pressed("ZAP!"):
