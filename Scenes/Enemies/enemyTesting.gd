b6d9a85e72443d728f478a75ee36d37053b26f39
navisraditya <navisraditya@gmail.com>
2024-03-12 00:00:47 +0700

    enemy moves

--- Scenes/Enemies/enemyTesting.gd
+++ Scenes/Enemies/enemyTesting.gd
@@ -2,41 +2,65 @@ extends KinematicBody2D
 
 var dir = Vector2.ZERO
 var cell_size
-var tilemap
+export(NodePath) var tilemap_path
+var tilemap = null
+var randomInteger
 
 func _ready():
-	tilemap = $"/root/Testing/TileMap"
-	cell_size = tilemap.cell_size
+	if tilemap_path:
+		tilemap = get_node(tilemap_path)
+		if tilemap:
+			cell_size = tilemap.cell_size
+			
+func _process(delta):
+	random_movement()
 
-func _input(event):
-	if event.is_action_pressed("ui_left"):
-		dir = Vector2(-1, -1)  # Diagonal movement for isometric left
-		move_player()
-
-	if event.is_action_pressed("ui_right"):
-		dir = Vector2(1, 1)  # Diagonal movement for isometric right
-		move_player()
-
-	if event.is_action_pressed("ui_up"):
-		dir = Vector2(1, -1)  # Diagonal movement for isometric up
-		move_player()
-
-	if event.is_action_pressed("ui_down"):
-		dir = Vector2(-1, 1)  # Diagonal movement for isometric down
-		move_player()
-
-func move_player():
-	var new_pos = self.position + (dir * cell_size / 2)
-	var con_pos = convert_to_tilemap_pos(new_pos)
-	var tile_id = tilemap.get_cellv(con_pos)
+func random_movement():
+	randomInteger = randi() % 4
 	
-	if tile_id in tilemap.walkable_tile_ids:
-		self.position = new_pos
+	if randomInteger == 0:
+		moveDown()
+	elif randomInteger == 1:
+		moveLeft()
+	elif randomInteger == 2:
+		moveRight()
+	elif randomInteger == 3:
+		moveUp()
+
+func moveLeft():
+	dir = Vector2(-1, -1)  # Diagonal movement for isometric left
+	move_enemy()
+	
+func moveRight():
+	dir = Vector2(1, 1)  # Diagonal movement for isometric right
+	move_enemy()
+	
+func moveUp():
+	dir = Vector2(1, -1)  # Diagonal movement for isometric up
+	move_enemy()
+
+func moveDown():
+	dir = Vector2(-1, 1)  # Diagonal movement for isometric down
+	move_enemy()
+
+func move_enemy():
+	if tilemap:
+		var new_pos = self.position + (dir * cell_size / 2)
+		var con_pos = convert_to_tilemap_pos(new_pos)
+		var tile_id = tilemap.get_cellv(con_pos)
+		
+		if tile_id in tilemap.walkable_tile_ids:
+			self.position = new_pos
+		else:
+			print("enemy Cannot move there")
 	else:
-		print("Cannot move there")
+		print("Tilemap not assigned.")
 
 func convert_to_tilemap_pos(pos: Vector2):
-	# Convert Cartesian coordinates to tilemap coordinates
-	var tilemap_origin = tilemap.world_to_map(Vector2.ZERO)
-	var tilemap_pos = tilemap.world_to_map(pos)
-	return tilemap_pos - tilemap_origin
+	if tilemap:
+		var tilemap_origin = tilemap.world_to_map(Vector2.ZERO)
+		var tilemap_pos = tilemap.world_to_map(pos)
+		return tilemap_pos - tilemap_origin
+	else:
+		print("Tilemap not assigned.")
+		return Vector2.ZERO
