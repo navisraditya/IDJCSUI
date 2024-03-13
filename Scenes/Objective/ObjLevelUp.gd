c823d87a700efe2b338ce019ebbfb0a3625352e4
navisraditya <navisraditya@gmail.com>
2024-03-13 09:51:00 +0700

    objective nodes refactor

--- Scenes/Objective/ObjLevelUp.gd
+++ Scenes/Objective/ObjLevelUp.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+var background
+
 func _on_ObjLevelUp_body_entered(body):
 	if body.get_name() == "Player":
-		print("obj up")
+	  print("gas")
+	  background = get_parent().get_node("Player/Camera2D/Node2D/ColorRect")
+	  background.change_background_color()
