a9be032140391522cebdb9ab956f081da6a61ca6
navisraditya <navisraditya@gmail.com>
2024-05-27 13:50:25 +0700

    splash screen changed

---
+++ Scenes/Scenery/SplashScreen.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+# Time to display the splash screen (in seconds)
+onready var slide1 = $Sprite
+onready var slide2 = $Sprite2
+onready var slide3 = $Sprite3
+
+var timer = Timer.new()
+
+func _ready():
+	add_child(timer)
+	timer.start(1)
+	timer.connect("timeout", self, "_on_timer_timeout")
+
+#	# Start a timer to switch scenes after display_time seconds
+#	yield(get_tree().create_timer(display_time), "timeout")
+#	get_tree().change_scene("res://path_to_your_main_scene.tscn")
+
+func first_slide():
+	slide1.visible = true
+	slide2.visible = false
+	slide3.visible = false
+	
+func second_slide():
+	slide1.visible = false
+	slide2.visible = true
+	slide3.visible = false
+	
+func third_slide():
+	slide1.visible = false
+	slide2.visible = false
+	slide3.visible = true
+	
+func _on_timer_timeout():
+	if slide1.visible == true:
+		second_slide()
+		timer.start(1)
+	elif slide2.visible == true:
+		third_slide()
+		timer.start(1)
+	elif slide3.visible == true:
+		timer.start(1)
+		get_tree().change_scene("res://Scenes/Scenery/LandingPage.tscn")
